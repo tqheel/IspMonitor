951eb4f7dd9b6b1919e08647c4a2c5f2094c1b13
Todd Qualls <todd.qualls@lexisnexis.com>
2017-04-07 17:23:45 -0400

    WIP on logging to file

--- IspMomitor/Program.cs
+++ IspMomitor/Program.cs
@@ -1,35 +1,65 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace IspMomitor
 {
     class Program
     {
-        static void Main(string[] args)
+        static async void MainAsync(string[] args)
         {
             Console.WriteLine("Hello World!");
             var url = "https://google.com";
-            var res =  MakeRequest(url).Result;
             Console.WriteLine($"Sending GET to {url}...");
+            await MakeRequest(url).ConfigureAwait(false);
+        }
+
+        private static async Task MakeRequest(string url)
+        {
+            var httpClient = new HttpClient();
+            var res = new HttpResponseMessage();
+            try
+            {
+                
+                res = await httpClient.GetAsync(url).ConfigureAwait(false);
+                await WriteSuccess(res).ConfigureAwait(false);                
+            }
+            catch (Exception ex)
+            {
+                await WriteFailure(ex.Message).ConfigureAwait(false);
+                WriteToConsole();
+            }
+            
+            WriteToConsole(res);
+            
+        }
+
+        private static Task WriteFailure(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void WriteToConsole(HttpResponseMessage res)
+        {
             Console.WriteLine($"The response was: {res.ToString()}.");
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
 
-        private static async Task<HttpResponseMessage> MakeRequest(string url)
+        private static async Task WriteSuccess(HttpResponseMessage message)
         {
-            var httpClient = new HttpClient();
-            return await httpClient.GetAsync(url).ConfigureAwait(false);
-
-        }
-
-        private void WriteLog(HttpResponseMessage message)
-        {
-            using (var writer = new StreamWriter())
+            const string filePath = @"C:\IspLog\log.txt";
+            FileStream fileStream = new FileStream(filePath, FileMode.Append);
+            using (var logFile = new StreamWriter(fileStream))
             {
-
+                var i = 0;
+                while (i < 10)
+                {
+                    i++;
+                    await logFile.WriteAsync($"This is iteration {i}:\n{message}\n");
+                    
+                }
             }
         }
     }
