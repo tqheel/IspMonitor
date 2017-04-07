caaf3cc8f3453e11d9ae7be4538064b8295fb662
Todd Qualls <tqualls@gmail.com>
2017-04-06 22:11:44 -0400

    Adds method for making an http request.

--- IspMomitor/Program.cs
+++ IspMomitor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace IspMomitor
 {
@@ -7,6 +9,19 @@ namespace IspMomitor
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var url = "https://google.com";
+            var res =  MakeRequest(url).Result;
+            Console.WriteLine($"Sending GET to {url}...");
+            Console.WriteLine($"The response was: {res.ToString()}.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static async Task<HttpResponseMessage> MakeRequest(string url)
+        {
+            var httpClient = new HttpClient();
+            return await httpClient.GetAsync(url).ConfigureAwait(false);
+
         }
     }
 }
